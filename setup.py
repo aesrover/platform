11599d76ad8ecf8e16019075907491ff250d6c1a
Lucien Gaitskell <lucieng@gaitskell.com>
2017-08-16 15:53:34 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='platform',
+      version='1.0',
+      description='Platform framework for autonomous movement and transducer data storage',
+      author='AESR',
+      url='https://github.com/aesrover',
+      packages=['platform']
+      )
