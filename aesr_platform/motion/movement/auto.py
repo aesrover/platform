Fix GPSScaleDiff scaling

aesr_platform/motion/movement/auto.py
@@ -19,7 +19,7 @@ class DynamicScaleDiff:
 
 class GPSScaleDiff(DynamicScaleDiff):
     def calc(self, t, cp):
-        return self._diff(t, cp)*np.array((math.cos(math.radians(cp[0])) * 111320., 110540.))
+        return self._diff(t, cp)*np.array((math.cos(math.radians(cp[1])) * 111320., 110540.))
 
 
 class _Mode: