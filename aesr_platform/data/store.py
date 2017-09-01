cf6c04f9560aed12885b7183ce382f1d9b4494b6
Lucien Gaitskell <lucieng@gaitskell.com>
2017-08-31 21:25:28 -0400

    Complete 'store' module

--- aesr_platform/data/store.py
+++ aesr_platform/data/store.py
@@ -10,21 +10,26 @@ from aesrdatabaselib.base.data import DataHandler
 
 
 class Store:
-    def __init__(self, log: Logger, transducers: List[Transducer]):
+    def __init__(self, log: Logger, dh: DataHandler, transducers: List[Transducer]):
+        self.log = log
         self.transducers = transducers
-        self.handlers = []
-
-    def add_handler(self, handler: DataHandler):
-        self.handlers.append(handler)
+        self.handler = dh
 
     def store(self):
+        data = []
         for t in self.transducers:
-            t.read()
+            try:
+                d = t.read()
+            except:
+                self.log.exception(t._add_transducer_info({'state': False}))
+            else:
+                data.append(d)
+        self.handler.store_many(data)
 
 
 class StoreThreaded(Store, Thread):
-    def __init__(self, log: Logger, transducers: List[Transducer], store_delay=1, **thread_kwargs):
-        super().__init__(log, transducers)
+    def __init__(self, log: Logger, dh: DataHandler, transducers: List[Transducer], store_delay=1, **thread_kwargs):
+        super().__init__(log, dh, transducers)
         Thread.__init__(self, **thread_kwargs)
         self.store_delay = store_delay
         self.running = False
