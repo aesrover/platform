d36714fe062d64bb44b10e8a5d247c5973a36b89
Lucien Gaitskell <lucieng@gaitskell.com>
2017-09-02 22:28:18 -0400

    Add all deploy

---
+++ deploy_all.py
@@ -0,0 +1,103 @@
+import logging
+import math
+
+from pymongo import MongoClient
+from log4mongo.handlers import MongoHandler
+
+from aesrdatabaselib.main_util import generateTimeName
+
+# Typing:
+from typing import List
+
+
+from aesrdevicelib.motion.blue_esc import BlueESC_PCA9685
+from aesrdevicelib.motion.pca9685 import PCA9685
+from aesrdevicelib.sensors.bno055 import BNO055, OPERATION_MODE_COMPASS
+from aesrdevicelib.sensors.tsys01 import TSYS01
+from aesrdevicelib.sensors.ms5837 import MS5837_30BA
+from aesrdevicelib.sensors.vernier_odo import VernierODO
+from aesrdevicelib.sensors.bme280 import BME280
+from aesrdevicelib.sensors.voltage_monitor import VoltageMonitorADS1115
+from aesrdevicelib.other.tca9548a import TCA9548A
+
+from aesrdevicelib.sensors.gps_read import GPSRead
+from aesrdatabaselib.mongo.waypoint import MongoWaypointManager
+from aesrdatabaselib.mongo.data import MongoDataHandler
+
+from aesr_platform.motion.movement.movement import ThrusterManager
+from aesr_platform.motion.movement.auto import AutoCalc, GPSScaleDiff
+from aesr_platform.motion.movement.movement import AutoThrustThreaded
+from aesr_platform.motion.movement.control_server.control_server import ControlServer
+from aesr_platform.data.store import StoreThreaded
+import time
+
+# AUTO CONFIG:
+AUTO_INIT_DB = 3
+AUTO_HOLD_DB = 5
+
+db_name = generateTimeName()
+print("Using Database: {}".format(db_name))
+
+MONGO_HOST = {'host': 'localhost', 'port': 27017}
+
+mongo_db = MongoClient(**MONGO_HOST)[db_name]
+
+log = logging.getLogger('AESR')
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.StreamHandler())  # Prints to console
+log.addHandler(MongoHandler(database_name=db_name, collection='log', **MONGO_HOST))
+log.info("Initialized logger.", extra={'type': 'START'})
+pca = PCA9685(0x40)
+pca.set_pwm_freq(300)
+ths = [
+    BlueESC_PCA9685((0, 1), 0, 12, pca)
+    , BlueESC_PCA9685((-1, 0), math.pi / 2, 13, pca)
+    , BlueESC_PCA9685((1, 0), math.pi / 2, 14, pca)
+    #, BlueESC_PCA9685((0, -1), 0, 15, pca)
+]
+
+wpm = MongoWaypointManager(mongo_db['wps'])
+
+pt = GPSRead()
+
+ht = BNO055(mode=OPERATION_MODE_COMPASS)
+try:
+    print("BEGIN IMU CALIBRATION:")
+    s = (0,) * 4
+    #while s[3] != 3:
+    while True:
+        s = ht.get_calibration_status()
+        print("\rIMU Calibration Status: {}".format(s), end='', flush=True)
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    print("\nSkipping IMU CALIBRATION")
+
+
+if True:
+    tca = TCA9548A()
+    transducers = [
+        VoltageMonitorADS1115(0, 0x49, resistor_p=336, resistor_n=100),
+        TSYS01(pre_func=tca.select_channel, pre_func_args=(0,)),
+        MS5837_30BA(pre_func=tca.select_channel, pre_func_args=(0,)),
+        VernierODO(),
+        BME280(pre_func=tca.select_channel, pre_func_args=(2,))
+        , pt
+    ]
+else:
+    transducers = []
+tm = ThrusterManager(ths)
+
+ac = AutoCalc(log, pt, ht, 7, AUTO_INIT_DB, AUTO_HOLD_DB, 2, min_m=0.5, rot_gain=1.5, dynamic_scale=GPSScaleDiff())
+thread_a = AutoThrustThreaded(log, tm, ac, wpm, rate=10, t_name="ThrusterCTL")
+
+st = StoreThreaded(log, MongoDataHandler(mongo_db['data']), transducers)
+
+if __name__ == "__main__":
+    st.start()
+    try:
+        cs = ControlServer('0.0.0.0', 8000, thread_a)
+        cs.run_server()
+    except:
+        tm.disable_thrusters()
+        st.close()
+        pt.close()
