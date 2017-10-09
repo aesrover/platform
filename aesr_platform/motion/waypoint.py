cb826b8fe7d7423dfdf8e23b8ddf5d3ae3e6d261
Lucien Gaitskell <lucieng@gaitskell.com>
2017-10-09 18:39:20 -0400

    Simplify curr pos and add auto state input to waypoint

--- aesr_platform/motion/waypoint.py
+++ aesr_platform/motion/waypoint.py
@@ -1,6 +1,8 @@
 from threading import Thread
 import time
 
+from typing import Callable, Union
+
 from aesrdatabaselib.base.waypoint import WaypointManager
 from aesrdevicelib.other.timed_motor import TimedControlledMotor
 
@@ -14,7 +16,7 @@ class WaypointSelector(Thread):
         self.depth_m = depth_m
 
         self.last_wp = None
-        self.curr_wp = None
+        self.is_auto = False
         self.on_target = None
         self.running = False
         self.start()
@@ -22,33 +24,38 @@ class WaypointSelector(Thread):
     def loop(self):
         """ Main loop. """
         while self.running:
-            if self.curr_wp is None:
-                self.curr_wp = self.wpm.select_next_wp()
+            if self._get_auto() is False:
+                time.sleep(0.1)
+                continue
 
-            if self.on_target is True and self.curr_wp is not None:
-                wait = self.curr_wp['time']
-                depth = self.curr_wp['depth']
+            if self.get_current_wp() is None:
+                self.wpm.select_next_wp()
+
+            if self.on_target is True and self.get_current_wp() is not None:
+                wait = self.get_current_wp()['time']
+                depth = self.get_current_wp()['depth']
 
                 # Move to desired depth:
                 self.depth_m.move_abs_tick(depth)
 
-                time.sleep(wait)  # Wait at waypoint
+                ts = time.time()
+                while (time.time() + ts) < wait and self._get_auto():
+                    time.sleep(0.05)
 
                 # Get next:
-                self.last_wp = self.curr_wp
+                self.last_wp = self.get_current_wp()
                 new_wp = self.wpm.select_next_wp()
                 if new_wp is not None:
                     # Move back to starting position if moving to new position for next waypoint
                     if not self.last_wp['pos'] == new_wp['pos']:
                         self.depth_m.move_abs_tick(0)
 
-                self.curr_wp = new_wp
                 self.on_target = False
             time.sleep(0.5)
 
     def start(self):
         """ Start thread. """
-        self.curr_wp = self.wpm.select_next_wp()
+        self.wpm.select_next_wp()
         self.on_target = False
         self.running = True
         super().start()
@@ -58,7 +65,15 @@ class WaypointSelector(Thread):
         self.on_target = True
 
     def get_current_wp(self):
-        return self.curr_wp
+        return self.wpm.get_curr_wp()
+
+    def set_auto(self, state: Union[bool, Callable[bool]]):
+        self.is_auto = state
+
+    def _get_auto(self):
+        if callable(self.is_auto):
+            return self.is_auto()
+        return self.is_auto
 
     def close(self):
         self.running = False
